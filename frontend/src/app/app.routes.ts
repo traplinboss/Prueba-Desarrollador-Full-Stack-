b562e056e1e5018d61505c5ea95a8179459f0f34
traplinboss <58180044+traplinboss@users.noreply.github.com>
2025-10-23 06:52:01 -0500

    Add files via upload

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { UsuariosListComponent } from './components/usuarios-list/usuarios-list';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: UsuariosListComponent
+  }
+];
